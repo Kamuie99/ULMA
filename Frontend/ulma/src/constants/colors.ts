5dd86b574fa5c8d74049771b943cfe6775d392ef
윤예리 <yoonr721@gmail.com>
2024-09-10 17:46:42 +0900

    Rename: 프론트 폴더 생성 및 수정2

---
+++ Frontend/ulma/src/constants/colors.ts
@@ -0,0 +1,14 @@
+const colors = {
+  WHITE: '#FFF',
+  BLACK: '#000',
+  GREEN_700: '#3FC89E',
+  GREEN_300: '#C2EADF', 
+  LIGHTGRAY: '#F7F8FA',
+  GRAY: '#A7A7A7',
+  PINK: '#F09AAA',
+  LIGHTPINK: '#F9CFD7',
+  YELLOW: '#FFF4C1',
+  BLUE: '#CEE9EB',
+};
+
+export {colors};

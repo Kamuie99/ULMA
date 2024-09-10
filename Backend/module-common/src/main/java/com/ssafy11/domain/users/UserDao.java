6619719ed6adc30d1686f91dacf45109e1b85554
kwondh5217 <trewq231@naver.com>
2024-09-10 16:45:39 +0900

    feat: 회원가입 기능 개발

--- Backend/module-common/src/main/java/com/ssafy11/domain/users/UserDao.java
+++ Backend/module-common/src/main/java/com/ssafy11/domain/users/UserDao.java
@@ -2,4 +2,7 @@ package com.ssafy11.domain.users;
 
 public interface UserDao {
 	Integer save(UserCommand command);
+	boolean existsByPhoneNumber(String phoneNumber);
+	boolean existsByEmail(String email);
+	boolean existsByLoginId(String loginId);
 }

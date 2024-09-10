b2a5a7d202fe0baffc60c6b1b098db464ba235c6
kwondh5217 <trewq231@naver.com>
2024-09-10 16:43:40 +0900

    feat: DTO 추가

---
+++ Backend/module-api/src/main/java/com/ssafy11/api/dto/UserJoinRequest.java
@@ -0,0 +1,12 @@
+package com.ssafy11.api.dto;
+
+import lombok.Builder;
+
+@Builder
+public record UserJoinRequest (String name,
+							   String loginId,
+							   String password,
+							   String passwordConfirm,
+							   String email,
+							   String phoneNumber){
+}

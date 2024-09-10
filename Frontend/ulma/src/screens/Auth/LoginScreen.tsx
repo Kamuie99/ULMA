21fa5ae76e07c8b6b2dfe5bab4c08d535d75e44e
윤예리 <yoonr721@gmail.com>
2024-09-10 17:45:15 +0900

    Rename: 프론트 폴더 생성 및 수정

---
+++ Frontend/ulma/src/screens/Auth/LoginScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+function LoginScreen() {
+  return (
+    <View>
+      <Text>hi</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default LoginScreen;

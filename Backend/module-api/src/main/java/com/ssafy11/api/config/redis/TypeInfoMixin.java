fee1578238abf90b51e857a833a5453e20e6632c
kwondh5217 <trewq231@naver.com>
2024-09-10 16:46:03 +0900

    feat: Redis 적용

---
+++ Backend/module-api/src/main/java/com/ssafy11/api/config/redis/TypeInfoMixin.java
@@ -0,0 +1,7 @@
+package com.ssafy11.api.config.redis;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
+public abstract class TypeInfoMixin {
+}

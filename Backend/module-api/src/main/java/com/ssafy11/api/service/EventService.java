c3abc39897840e068ae345294c27ec06e56853f1
최민 <rmdwjd76@naver.com>
2024-09-13 08:17:54 +0900

    Style : 의존성 주입 방식 변경

--- Backend/module-api/src/main/java/com/ssafy11/api/service/EventService.java
+++ Backend/module-api/src/main/java/com/ssafy11/api/service/EventService.java
@@ -5,41 +5,37 @@ import com.ssafy11.domain.events.EventCommand;
 import com.ssafy11.domain.events.EventDao;
 import com.ssafy11.domain.events.dto.Event;
 import com.ssafy11.domain.common.PaginatedResponse;
-import com.ssafy11.domain.participant.dto.Participant;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.ssafy11.domain.participant.dto.EventParticipant;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 @Service
+@RequiredArgsConstructor
 @Transactional
 public class EventService {
     private final EventDao eventDao;
-
-    @Autowired
-    public EventService(EventDao eventDao) {
-        this.eventDao = eventDao;
-    }
 
     public Integer addEvent(EventCommand event) {
         Assert.notNull(event, "Event must not be null");
         return eventDao.addEvent(event);
     }
 
+    @Transactional(readOnly = true)
     public PaginatedResponse<Event> getEvents(Integer userId, PageDto pageDto) {
         Assert.notNull(userId, "User must not be null");
-
         PaginatedResponse<Event> EventsList = eventDao.getEvents(userId, pageDto);
         return EventsList;
 
     }
 
-    public PaginatedResponse<Participant> getEvent(Integer eventId, PageDto pageDto) {
+    @Transactional(readOnly = true)
+    public PaginatedResponse<EventParticipant> getEvent(Integer eventId, PageDto pageDto) {
         Assert.notNull(eventId, "Event must not be null");
 
-        PaginatedResponse<Participant> guestsList = eventDao.getEvent(eventId, pageDto);
+        PaginatedResponse<EventParticipant> guestsList = eventDao.getEvent(eventId, pageDto);
         return guestsList;
     }
-
 
 }

53c5b347071db363a4c435cdea5323e77a376292
윤예리 <yoonr721@gmail.com>
2024-09-12 12:33:58 +0900

    Test: 임시저장

--- Frontend/ulma/App.tsx
+++ Frontend/ulma/App.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
+import SignupScreen from '@/screens/Auth/SignupScreen';
+import LoginScreen from '@/screens/Auth/LoginScreen';
 
 function App() {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      {/* <SignupScreen /> */}
+      <LoginScreen />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
   },
 });
 

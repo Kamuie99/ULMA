209160e42c553c17913525bdb41d6ddb575f43f9
최민 <rmdwjd76@naver.com>
2024-09-13 08:17:54 +0900

    Style : 의존성 주입 방식 변경

--- Backend/module-api/src/main/java/com/ssafy11/api/controller/ParticipantController.java
+++ Backend/module-api/src/main/java/com/ssafy11/api/controller/ParticipantController.java
@@ -3,48 +3,56 @@ package com.ssafy11.api.controller;
 import com.ssafy11.domain.common.PageDto;
 import com.ssafy11.domain.common.PaginatedResponse;
 import com.ssafy11.api.service.ParticipantService;
+import com.ssafy11.domain.participant.dto.Participant;
 import com.ssafy11.domain.participant.dto.Transaction;
 import com.ssafy11.domain.participant.dto.UserRelation;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/participant")
 public class ParticipantController {
 
     private final ParticipantService participantService;
 
-    @Autowired
-    public ParticipantController(ParticipantService participantService) {
-        this.participantService = participantService;
-    }
-
-    //동일 이름 검증
+    //동명이인
     @GetMapping("/same/{userId}")
     public ResponseEntity<?> sameName(@PathVariable("userId") Integer userId, @RequestParam("name") String name) {
+        Assert.notNull(userId, "userId must not be null");
+        Assert.notNull(name, "name must not be null");
         List<UserRelation> userRelationList = participantService.sameName(userId, name);
         return ResponseEntity.ok(userRelationList);
     }
 
-    //송금 내역 확인
+    //등록된 지인과 거래 내역
     @GetMapping("/{userId}/{guestID}")
     public ResponseEntity<?> getTransactions(@PathVariable("userId") Integer userId,
                                             @PathVariable("guestID") Integer guestId,
                                             @ModelAttribute PageDto pagedto) {
+        Assert.notNull(userId, "userId must not be null");
+        Assert.notNull(guestId, "guestID must not be null");
         PaginatedResponse<Transaction> transactions = participantService.getTransactions(userId, guestId, pagedto);
         return ResponseEntity.ok(transactions);
     }
 
-    //게스트 추가
-//    @PostMapping()
-//    public ResponseEntity<?> addGuests(@RequestBody Guests guest) {
-//        Integer guestId
-//    }
+    //경조사비 추가
+    @PostMapping("/money")
+    public ResponseEntity<?> addParticipant(@RequestBody Participant participant) {
+        Assert.notNull(participant, "participant must not be null");
+        Integer resultId = participantService.addParticipant(participant);
+        return ResponseEntity.ok(resultId);
+    }
 
-    //경조사 추가
+    //지인 등록 /api/participant
+
+    //등록된 지인 정보 /api/participant/{user_id}
+
+
 
 
 }

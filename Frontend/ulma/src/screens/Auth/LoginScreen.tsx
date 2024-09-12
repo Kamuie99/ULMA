949b083d8cb3e8a0b1cbcfe5db7fc354405444c3
윤예리 <yoonr721@gmail.com>
2024-09-12 12:33:58 +0900

    Test: 임시저장

--- Frontend/ulma/src/screens/Auth/LoginScreen.tsx
+++ Frontend/ulma/src/screens/Auth/LoginScreen.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import TitleTextField from '@/components/common/TitleTextField';
 
 function LoginScreen() {
+  const EventName = '남자친구 생일';
   return (
     <View>
-      <Text>hi</Text>
+      <TitleTextField
+        frontLabel="AI가 추천하는 "
+        emphMsg={EventName}
+        backLabel=" 메세지예요."
+      />
     </View>
   );
 }

a0076a1500bf7a92b0b8c47c202111ea40bb57b9
윤예리 <yoonr721@gmail.com>
2024-09-20 14:09:03 +0900

    Feat: 내비게이션 추가
    - 링크 연결
    - Tabbar 추가
    - 페이지 연결 확인중

---
+++ Frontend/ulma/src/screens/Setting/SettingHomeScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+interface SettingHomeScreenProps {}
+
+function SettingHomeScreen({}: SettingHomeScreenProps) {
+  return (
+    <View>
+      <Text>셋팅 홈</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default SettingHomeScreen;

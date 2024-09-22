0f95c7c079da6758a08eca89aaccda771f6820ad
최민 <rmdwjd76@naver.com>
2024-09-23 07:27:38 +0900

    Refactor : PageResponse의 명칭 변경에 따른 변경

--- Backend/module-common/src/main/java/com/ssafy11/domain/events/EventDao.java
+++ Backend/module-common/src/main/java/com/ssafy11/domain/events/EventDao.java
@@ -2,11 +2,12 @@ package com.ssafy11.domain.events;
 
 import com.ssafy11.domain.common.PageDto;
 import com.ssafy11.domain.events.dto.Event;
-import com.ssafy11.domain.common.PaginatedResponse;
+import com.ssafy11.domain.common.PageResponse;
 import com.ssafy11.domain.participant.dto.EventParticipant;
 
 public interface EventDao {
     Integer addEvent(EventCommand event);
-    PaginatedResponse<Event> getEvents(Integer userId, PageDto pageDto);
-    PaginatedResponse<EventParticipant> getEvent(Integer eventId, PageDto pageDto);
+    PageResponse<Event> getEvents(Integer userId, PageDto pageDto);
+    PageResponse<EventParticipant> getEvent(Integer eventId, PageDto pageDto);
+    Integer getEventByUserId(Integer eventId);
 }

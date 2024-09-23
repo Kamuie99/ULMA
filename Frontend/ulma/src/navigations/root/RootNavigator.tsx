565cd3a0aea8ca7046cdee5eff6bfd6290351537
윤예리 <yoonr721@gmail.com>
2024-09-23 15:09:39 +0900

    Home Page 추가

--- Frontend/ulma/src/navigations/root/RootNavigator.tsx
+++ Frontend/ulma/src/navigations/root/RootNavigator.tsx
@@ -3,7 +3,6 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import AuthStackNavigator from '../stack/AuthStackNavigator';
 import EventStackNavigator from '../stack/EventStackNavigator';
 import PayStackNavigator from '../stack/PayStackNavigator';
-import {NavigationContainer} from '@react-navigation/native';
 import SettingStackNavigator from '../stack/SettingStackNavigator';
 import HomeStackNavigator from '../stack/HomeStackNavigator';
 
@@ -18,7 +17,7 @@ function RootNavigator() {
       <Tab.Screen name="Home" component={HomeStackNavigator} />
       <Tab.Screen name="Auth" component={AuthStackNavigator} />
       <Tab.Screen name="Pay" component={PayStackNavigator} />
-      <Tab.Screen name="Event" component={EventStackNavigator} />
+      {/* <Tab.Screen name="Event" component={EventStackNavigator} /> */}
       <Tab.Screen name="Setting" component={SettingStackNavigator} />
     </Tab.Navigator>
   );

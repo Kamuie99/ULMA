5424064045e44f2a38685ebe6465ae63a4864b88
최민 <rmdwjd76@naver.com>
2024-09-26 17:57:09 +0900

    fix: 소나큐브 에로사항 수정

--- Backend/module-common/src/main/java/com/ssafy11/domain/participant/dto/AddGuestResponse.java
+++ Backend/module-common/src/main/java/com/ssafy11/domain/participant/dto/AddGuestResponse.java
@@ -1,10 +1,4 @@
 package com.ssafy11.domain.participant.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Service;
-
 public record AddGuestResponse (String name, String category){
 }

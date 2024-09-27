9ae2ebc572144b28bf8c33191504ecddb31eb8bc
최민 <rmdwjd76@naver.com>
2024-09-28 02:34:50 +0900

    fix : 쓰지 않는 코드 제거

--- Backend/module-common/src/main/java/com/ssafy11/domain/events/EventDaoImpl.java
+++ Backend/module-common/src/main/java/com/ssafy11/domain/events/EventDaoImpl.java
@@ -17,7 +17,6 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 import static com.ssafy11.ulma.generated.Tables.*;
 

7de20f438343644dc00bd51129a4829a526ea738
최민 <rmdwjd76@naver.com>
2024-09-28 03:27:32 +0900

    fix : 필요없는 코드 삭제

--- Backend/module-common/src/main/java/com/ssafy11/domain/participant/dto/Participant.java
+++ Backend/module-common/src/main/java/com/ssafy11/domain/participant/dto/Participant.java
@@ -1,9 +1,6 @@
 package com.ssafy11.domain.participant.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Builder
 public record Participant(

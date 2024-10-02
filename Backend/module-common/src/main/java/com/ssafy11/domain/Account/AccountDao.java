b78bda3dcc1067ae14323bbd01022b483f8d4e54
이승민 <lsm5631@naver.com>
2024-10-02 12:07:18 +0900

    Feat : 계좌 거래 내역 생성

--- Backend/module-common/src/main/java/com/ssafy11/domain/Account/AccountDao.java
+++ Backend/module-common/src/main/java/com/ssafy11/domain/Account/AccountDao.java
@@ -1,12 +1,24 @@
 package com.ssafy11.domain.Account;
 
+import com.ssafy11.domain.Pay.PayHistory;
+
+import java.time.LocalDate;
 import java.util.List;
 
 public interface AccountDao {
     Account createAccount(Integer userId, String bankCode);  // BankCode -> String
+
     Account connectAccount(Integer userid, String accountNumber);
+
     List<Account> findAllAccounts(Integer userId, String bankCode);  // BankCode -> String
+
     Account connectedAccount(Integer userId);
+
     Account findByAccountNumber(String accountNumber);
+
     Account findByAccountId(Integer accountId);
+
+    PayHistory sendMoney(String senderAccountNumber, String info, String targetAccountNumber, Long amount);
+    PayHistory chargeBalance(String accountNumber, Long amount);
+    List<PayHistory> findPayHistory(String accountNumber, LocalDate startDate, LocalDate endDate, String payType);
 }

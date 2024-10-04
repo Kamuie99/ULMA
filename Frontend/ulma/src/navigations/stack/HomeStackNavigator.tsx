2994056341379683c7fe605bb430781f94c4659c
윤예리 <yoonr721@gmail.com>
2024-10-04 14:43:16 +0900

    [HOTFIX] SyntaxError 수정

--- Frontend/ulma/src/navigations/stack/HomeStackNavigator.tsx
+++ Frontend/ulma/src/navigations/stack/HomeStackNavigator.tsx
@@ -1,20 +1,17 @@
+import {homeNavigations} from '@/constants/navigations';
+import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import { TouchableOpacity } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
-import { colors } from '@/constants';
-import { homeNavigations } from '@/constants/navigations';
+import {StyleSheet, View} from 'react-native';
 
+import {colors} from '@/constants';
+import Icon from 'react-native-vector-icons/Entypo';
+import LandingPage from '@/screens/Home/LandingScreen';
 import ScheduleMainScreen from '@/screens/Home/ScheduleMainScreen';
-import ScheduleAddScreen from '@/screens/Home/ScheduleAddScreen';
-import FriendsListScreen from '@/screens/Freinds/FriendsListScreen';
-import FriendsDetailScreen from '@/screens/Freinds/FriendsDeatilScreen';
 
 export type homeStackParamList = {
+  [homeNavigations.HOME]: undefined;
+  [homeNavigations.LANDING]: undefined;
   [homeNavigations.SCHEDULE_MAIN]: undefined;
-  [homeNavigations.SCHEDULE_ADD]: { selectedUser?: { guestId: number; name: string } };
-  [homeNavigations.SELECT_FRIEND]: undefined;
-  [homeNavigations.FRIENDS_DETAIL]: { guestId: number }; // guestId를 받아오는 디테일 페이지
 };
 
 const Stack = createStackNavigator<homeStackParamList>();
@@ -23,7 +20,6 @@ function HomeStackNavigator() {
   return (
     <Stack.Navigator
       screenOptions={{
-<<<<<<< HEAD
         cardStyle: {
           backgroundColor: colors.WHITE,
         },
@@ -37,60 +33,17 @@ function HomeStackNavigator() {
         },
         headerTintColor: colors.BLACK,
       }}>
-=======
-        cardStyle: { backgroundColor: colors.WHITE },
-        headerStyle: { backgroundColor: colors.WHITE },
-        headerTitleAlign: 'left',
-        headerTitleStyle: { fontSize: 18, fontWeight: 'bold' },
-        headerTintColor: colors.BLACK,
-      }}
-    >
->>>>>>> 0634bef121e43bef7cf4344f053a62853915a7ee
       <Stack.Screen
         name={homeNavigations.SCHEDULE_MAIN}
         component={ScheduleMainScreen}
-        options={({ navigation }) => ({
+        options={{
           headerTitle: '경조사 일정 관리',
-          headerRight: () => (
-            <TouchableOpacity
-              onPress={() => navigation.navigate(homeNavigations.SCHEDULE_ADD)}
-              style={{ marginRight: 15 }}
-            >
-              <Icon name="add" size={24} color={colors.BLACK} />
-            </TouchableOpacity>
-          ),
-        })}
-      />
-      <Stack.Screen 
-        name={homeNavigations.SCHEDULE_ADD}
-        component={ScheduleAddScreen}
-        options={{
-          headerTitle: '경조사 일정 추가',
-        }}
-      />
-      <Stack.Screen
-        name={homeNavigations.SELECT_FRIEND}
-        component={FriendsListScreen}
-        options={{
-          headerTitle: '지인 선택',
-        }}
-      />
-      <Stack.Screen
-        name={homeNavigations.FRIENDS_DETAIL}
-        component={FriendsDetailScreen}
-        options={{
-          headerTitle: '거래내역 조회',
-        }}
-      />
-      <Stack.Screen
-        name={homeNavigations.LANDING} // LandingPage 추가
-        component={LandingPage}
-        options={{
-          headerTitle: '홈',
         }}
       />
     </Stack.Navigator>
   );
 }
+
+const styles = StyleSheet.create({});
 
 export default HomeStackNavigator;

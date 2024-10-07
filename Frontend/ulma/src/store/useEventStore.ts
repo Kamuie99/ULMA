c621e324ad7789f4371c00890d4148b3873e7d1a
윤예리 <yoonr721@gmail.com>
2024-10-08 06:34:29 +0900

    [feat] EventStore 생성

---
+++ Frontend/ulma/src/store/useEventStore.ts
@@ -0,0 +1,39 @@
+import {create} from 'zustand';
+
+interface Transaction {
+  id: string;
+  name: string;
+  amount: string;
+  selected: boolean;
+  transactionType: string;
+  eventID: string;
+}
+
+interface EventStore {
+  selectedTransactions: Transaction[];
+  eventID: string | null;
+  setSelectedTransactions: (transactions: Transaction[]) => void;
+  setEventID: (eventID: string) => void;
+}
+
+const useEventStore = create<EventStore>(set => ({
+  selectedTransactions: [],
+  eventID: null,
+
+  setSelectedTransactions: (transactions: Transaction[]) =>
+    set(state => ({
+      ...state,
+      selectedTransactions: transactions,
+    })),
+
+  setEventID: (eventID: string) =>
+    set(state => {
+      console.log(eventID);
+      return {
+        ...state,
+        eventID,
+      };
+    }),
+}));
+
+export default useEventStore;
